06f9525112fe8bc0bab083da970f3fc4f011ef3a
Kian <kian.info78@gmail.com>
2020-05-28 13:10:32 +1000

    prepend

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -1,25 +1,38 @@
-class LinkedLis{
-     
+
+class LinkedList {
     constructor(value) {
-        this.head = {
-            value: value,
-            next: null
-        }
-        this.tail = this.head
-        
+     this.head ={
+       value: value,
+       next: null
+     }
+     this.tail = this.head
     }
-    
-    append(value){
-        const newNode = {
-            value: value,
-            next: null
-        } 
-       this.tail.next = newNode;
-       this.tail = newNode; 
-    }
-
-}
-
-const myLinkedList = new LinkedLis(10);
-myLinkedList.append(5)
-console.log(myLinkedList)
+   append(value){
+     const newNode = {
+       value: value,
+       next: null
+     };
+     this.tail.next = newNode;
+     this.tail = newNode;
+     return this
+  
+   }
+   prepend(value){
+     const newNode={
+       value: value,
+       next: null
+     }
+    newNode.next = this.head
+     this.head = newNode 
+     return this 
+   }
+  }
+  
+  let myLinkedList = new LinkedList(10);
+  myLinkedList.append(5);
+  myLinkedList.prepend(16);
+  console.log(myLinkedList)
+  
+  
+  
+  

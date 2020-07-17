631c09fbc2197becd81ca14218871f2fe8185f45
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-07-17 21:09:38 +1000

    reverseWord

--- Array/reverseWord.js
+++ Array/reverseWord.js
@@ -5,10 +5,50 @@
 //  The message has been mostly deciphered, but all the words are backward! Your colleagues have handed off the last step to you.
 
 // Write a function reverseWords() that takes a message as an array of characters and reverses the order of the words in place.
-function reverseCharacters(message) {
+// function reverseCharacters(message) {
 
-    let leftIndex = 0;
-    let rightIndex = message.length - 1;
+//     let leftIndex = 0;
+//     let rightIndex = message.length - 1;
+  
+//     // Walk towards the middle, from both sides
+//     while (leftIndex < rightIndex) {
+  
+//       // Swap the left char and right char
+//       const temp = message[leftIndex];
+//       message[leftIndex] = message[rightIndex];
+//       message[rightIndex] = temp;
+//       leftIndex++;
+//       rightIndex--;
+//     }
+//   }
+
+  function reverseWords(message) {
+
+    // First we reverse all the characters in the entire message
+    reverseCharacters(message, 0, message.length - 1);
+    // This gives us the right word order
+    // but with each word backward
+  
+    // Now we'll make the words forward again
+    // by reversing each word's characters
+  
+    // We hold the index of the *start* of the current word
+    // as we look for the *end* of the current word
+    let currentWordStartIndex = 0;
+    for (let i = 0; i <= message.length; i++) {
+  
+      // Found the end of the current word!
+      if (i === message.length || message[i] === ' ') {
+  
+        // If we haven't exhausted the string our
+        // next word's start is one character ahead
+        reverseCharacters(message, currentWordStartIndex, i - 1);
+        currentWordStartIndex = i + 1;
+      }
+    }
+  }
+  
+  function reverseCharacters(message, leftIndex, rightIndex) {
   
     // Walk towards the middle, from both sides
     while (leftIndex < rightIndex) {
